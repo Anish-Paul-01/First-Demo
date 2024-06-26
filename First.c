0b5c774cca7555dacb80956e2cd4c8864ddc92f2
Anish Paul <anishpaul62422@gmail.com>
2024-06-26 10:24:45 +0530

    Adding Some Comments

--- First.c
+++ First.c
@@ -1,5 +1,7 @@
 #include<stdio.h>
 
+//This is a demo code for the first Repository
+
 int main(int argc, char const *argv[])
 {
     int n;
@@ -8,11 +10,14 @@ int main(int argc, char const *argv[])
     for (int i = 0; i < n; i++)
     {
         printf("Hello World! \n");
+        printf("/n");
     }
     printf("We are Going to say 'Namaste World!' \n");
+    printf("/n");
      for (int i = 0; i < (2*n); i++)
     {
         printf("Namaste World! This is INDIA \n");
+        printf("/n");
     }
 
     return 0;
