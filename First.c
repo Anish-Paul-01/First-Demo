2ea70ee5151643367d0e7bf643b08ff26e15d860
Anish Paul <anishpaul62422@gmail.com>
2024-06-26 10:30:26 +0530

    Some Small Changes

--- First.c
+++ First.c
@@ -1,23 +1,23 @@
-#include<stdio.h>
+#include <stdio.h>
 
-//This is a demo code for the first Repository
+// This is a demo code for the first Repository
 
 int main(int argc, char const *argv[])
 {
     int n;
     printf("Enter any number to repeat 'Hello World!': ");
-    scanf("%d",&n);
+    scanf("%d", &n);
     for (int i = 0; i < n; i++)
     {
         printf("Hello World! \n");
-        printf("/n");
+        printf("\n");
     }
     printf("We are Going to say 'Namaste World!' \n");
-    printf("/n");
-     for (int i = 0; i < (2*n); i++)
+    printf("\n");
+    for (int i = 0; i < (2 * n); i++)
     {
         printf("Namaste World! This is INDIA \n");
-        printf("/n");
+        printf("\n");
     }
 
     return 0;
